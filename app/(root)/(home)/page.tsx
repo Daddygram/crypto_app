e966061a2581d800c2c624315777651b0e172558
Joseph Moninyo <tsotne.s.2004@gmail.com>
2023-11-13 13:12:21 +0400

    first-commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,20 @@
+import MarketSection from '@/components/MarketSection'
+import SearchForm from '@/components/SearchForm'
+// import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
     <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
       <section className='nav-padding w-full'>
-        <div>
-          <h1 className='heading2 sm:heading1 lg:text-7xl mb-6 text-center text-white uppercase'>
+        <div className='flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
+          {/* <Image src="bitcoin.svg" width={60} height={60} alt='bitcoin' className='float top-150px left-20px' /> */}
+          <h1 className='text-outline heading2 sm:heading1 lg:text-7xl mb-6 text-center text-white uppercase'>
             Track and trade <br /> <span>Crypto Currencies</span>
           </h1>
         </div>
+        <SearchForm />
+        <MarketSection />
       </section>
     </main>
   )
